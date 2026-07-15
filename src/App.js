da774198eeb34360dce0b7b41ff9dade03a76721
Eslam Ahmed <eslam.ahmed.mohamad@gmail.com>
2026-07-15 21:04:12 +0300

    Put the basic elements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
+      <h2>To Do List</h2>
+      <ul>
+        
+      </ul>
+      <input />
+      <button>Add</button>
     </div>
   );
 }
