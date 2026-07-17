5b89f7b90a4da2a15b4cc5da55fc08ed74a36a8d
Eslam Ahmed <eslam.ahmed.mohamad@gmail.com>
2026-07-17 15:22:34 +0300

    Completed & Uncompleted task logic

--- src/App.js
+++ src/App.js
@@ -12,10 +12,25 @@ function App() {
   // Get the input value after press button
   const handleAddTodo = () => {
     const text = inputRef.current.value;
-    setToDos([...toDos, text]);
+
+    if (!text.trim()) return;
+
+    const newItem = {
+      completed: false,
+      text,
+    };
+
+    setToDos([...toDos, newItem]);
 
     // Delete the input value after add it
     inputRef.current.value = "";
+  };
+
+  // Toggle completed
+  const handleItemDone = (index) => {
+    const newTodos = [...toDos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setToDos(newTodos);
   };
 
   return (
@@ -23,11 +38,21 @@ function App() {
       {/* Todo List */}
       <div className="to-do-container">
         <h2>To Do List</h2>
+
         <ul>
-          {toDos.map((item) => (
-            <li>{item}</li>
-          ))}
+          {toDos.map((item, index) => {
+            return (
+              <li
+                key={index}
+                className={item.completed ? "done" : ""}
+                onClick={() => handleItemDone(index)}
+              >
+                {item.text}
+              </li>
+            );
+          })}
         </ul>
+
         {/* Input & Button */}
         <input ref={inputRef} placeholder="Enter Your Task.." />
         <button onClick={handleAddTodo}>Add</button>
